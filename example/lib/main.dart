0b044921eea255e31d111914957dbd22ea12258f
Rehmat Singh Gill <rehmatsinghgill@gmail.com>
2023-01-15 04:32:57 -0800

    Minor Fixes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
 
   @override
@@ -19,26 +18,21 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Material Symbols',
       ),
-      home: const MyHomePage(
-        title: 'Material Symbols'
-      ),
+      home: const MyHomePage(title: 'Material Symbols'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
-
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final ScrollController _scrollController = ScrollController();
 
   @override
@@ -52,66 +46,65 @@ class _MyHomePageState extends State<MyHomePage> {
             title: const Text('Material Symbols'),
           ),
           SliverPadding(
-            padding: const EdgeInsets.symmetric(
-            ),
+            padding: const EdgeInsets.symmetric(),
             sliver: SliverToBoxAdapter(
               child: Card(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 12,
-                  vertical: 12,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
+                  margin: const EdgeInsets.symmetric(
                     horizontal: 12,
-                    vertical: 12
+                    vertical: 12,
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        child: Text(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                            child: Text(
                           'For a full list of all available symbols, see fonts.google.com',
                           style: Theme.of(context).textTheme.bodyMedium,
                           maxLines: 3,
-                        )
-                      ),
-                      SizedBox.square(
-                        dimension: 40,
-                        child: IconButton(
-                          onPressed: () {
-                            launchUrl(Uri.parse('https://fonts.google.com/icons?icon.set=Material+Symbols'));
-                          },
-                          icon: const Icon(MaterialSymbols.open_in_new),
-                          style: IconButton.styleFrom(
-                            foregroundColor: colors.onSecondaryContainer,
-                            backgroundColor: colors.secondaryContainer,
-                            disabledBackgroundColor: colors.onSurface.withOpacity(0.12),
-                            hoverColor: colors.onSecondaryContainer.withOpacity(0.08),
-                            focusColor: colors.onSecondaryContainer.withOpacity(0.12),
-                            highlightColor: colors.onSecondaryContainer.withOpacity(0.12),
+                        )),
+                        SizedBox.square(
+                          dimension: 40,
+                          child: IconButton(
+                            onPressed: () {
+                              launchUrl(Uri.parse(
+                                  'https://fonts.google.com/icons?icon.set=Material+Symbols'));
+                            },
+                            icon: const Icon(MaterialSymbols.open_in_new),
+                            style: IconButton.styleFrom(
+                              foregroundColor: colors.onSecondaryContainer,
+                              backgroundColor: colors.secondaryContainer,
+                              disabledBackgroundColor:
+                                  colors.onSurface.withOpacity(0.12),
+                              hoverColor:
+                                  colors.onSecondaryContainer.withOpacity(0.08),
+                              focusColor:
+                                  colors.onSecondaryContainer.withOpacity(0.12),
+                              highlightColor:
+                                  colors.onSecondaryContainer.withOpacity(0.12),
+                            ),
                           ),
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ),
+                        )
+                      ],
+                    ),
+                  )),
             ),
           ),
           SliverGrid(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) => Center(
-                child: Icon(
-                  MaterialSymbols.all.values.elementAt(index),
-                  size: 40,
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => Center(
+                  child: Icon(
+                    MaterialSymbols.all.values.elementAt(index),
+                    size: 40,
+                  ),
                 ),
+                childCount: MaterialSymbols.all.length,
               ),
-              childCount: MaterialSymbols.all.length,
-            ),
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 100,
-            )
-          )
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 100,
+              ))
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -127,5 +120,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-
 }
